abc961ac12d1eb35266bcd89d163caf4135308a5
jldjld <jldsimplon@gmail.com>
2021-03-16 09:50:21 +0100

    Merge with main

--- vue.config.js
+++ vue.config.js
@@ -3,21 +3,4 @@ module.exports = {
     //process.env.NODE_ENV === 'production'
     //    ? ''
     //    : ''
-
-    module: {
-        rules: [
-            // ... other rules omitted
-
-            // this will apply to both plain `.scss` files
-            // AND `<style lang="scss">` blocks in `.vue` files
-            {
-                test: /\.scss$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
 }
