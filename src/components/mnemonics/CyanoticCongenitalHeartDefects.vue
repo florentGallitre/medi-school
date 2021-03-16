2feaed6f3efba071ed10647c076d9d3a9720172c
jldjld <jldsimplon@gmail.com>
2021-03-16 12:19:15 +0100

    Update data json file and start adding new components

---
+++ src/components/mnemonics/CyanoticCongenitalHeartDefects.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="CyanoticCongenitalHeartDefects">
+
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class CyanoticCongenitalHeartDefects extends Vue {}
+</script>
+
+<style>
+
+</style>
